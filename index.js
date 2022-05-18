97fc9b26cdecb6bb97d7b030dc08a0dbbabebfee
Norman Smart <normansmart14@gmail.com>
2022-05-18 16:21:15 -0400

    commit tree 198635a06b0554ecdfb7538d5c082f5dd05a87c2

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Write your solution in this file!
+const employee = {
+
+    name: "James",
+
+    streetAddress: "42 Broadway",
+
+
+
+};
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+
+
+    const newObj = { ...employee };
+
+    newObj[key] = value;
+  
+    return newObj;
+
+ 
+
+}
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+
+
+    obj[key] = value; 
+
+    return obj;
+
+ 
+
+}
+
+
+function deleteFromEmployeeByKey(obj, key, value){
+
+
+    return {
+        ...obj,
+        [key]: value,
+      };
+ 
+
+}
+
+
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+
+
+   delete employee.name;
+
+   return employee;
+ 
+
+}
